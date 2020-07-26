b53c923acb0d218858cdfe40074398a7da7bb1ae
Sang <sangbui1281@gmail.com>
2020-07-26 17:46:32 -0600

    sample nav bar added

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,54 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body: 
+        Center(child:
+          Column(children: [
+            Container(
+              margin: EdgeInsets.all(24),
+              width: 1612,
+              height: 56,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Color(0xFFDEE4E7)
+                 
+              ),
+              child: Row(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 12),
+                  child:
+                    Image.asset('', scale: 16)
+                ),
+                Container(
+                  height: 36,
+                  width: 86,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue
+                  ),
+                  margin: EdgeInsets.only(left: 36),
+                  child: Center(child: Text("Statistics")),
+                ),
+                Container(
+                  height: 36,
+                  width: 86,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue
+                  ),
+                  margin: EdgeInsets.only(left: 26),
+                  child: Center(child: Text("Areas")),
+                ),
+
+              ],),
+            
+        ),
+
+        ],) 
+        ,)
+        
+
       
     );
   }
